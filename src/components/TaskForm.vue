a271bf80c876e9bed129f1b690f8985a4d5b3721
Dat NGUYEN <datle@itc-admin-mm-015s-Mac-mini.local>
2019-01-29 17:32:18 +0700

    Completed Filter Features

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -16,8 +16,8 @@
         </div>
         <label>Status :</label>
         <select v-model="newTask.status" class="form-control" name="status">
-          <option value=true>To Do</option>
-          <option value=false>Completed</option>
+          <option value="true">To Do</option>
+          <option value="false">Completed</option>
         </select>
         <br>
         <div class="text-center">
@@ -37,53 +37,54 @@
 
 <script>
 export default {
+  // console.log(this.editTask)
   name: "TaskForm",
   data() {
-      return {
-          newTask: {
-              id: '',
-              name: '',
-              status: true
-          },
-          a: {}  
+    return {
+      newTask: {
+        id: "",
+        name: "",
+        status: true
       }
+    };
   },
+  props: {},
   computed: {
-      editTask(){
-          this.a = this.$store.state.editTask;
-          return this.$store.state.editTask;
-      }
+    editTask() {
+      return this.$store.state.editTask;
+    }
   },
   beforeMount() {
-    this.editTask;
-    console.log(this.a);
-    this.newTask.id = this.editTask.id;
-    this.newTask.name = this.editTask.name;
-    this.newTask.status = this.editTask.status;
-  },
-  beforeUpdate() {
-    console.log('akak');
+    console.log(this.editTask);
+    if (this.editTask) {
+      this.newTask.id = this.editTask.id;
+      this.newTask.name = this.editTask.name;
+      this.newTask.status = this.editTask.status;
+    } else {
+      this.newTask.id = '';
+      this.newTask.name = '';
+      this.newTask.status = true;
+    }
   },
   methods: {
-      closeForm(){
-          this.$store.commit('toggleForm');
-          console.log(this.editTask.status);
-      },
-      clearForm(){
-          this.newTask.id = this.newTask.name = '';
-          this.newTask.status = true;
-      },
-      addNewTask(){
-          const id = require("crypto-random-string");
-          if( this.newTask.status === 'false'){
-              this.newTask.status = false;
-          } else {
-              this.newTask.status = true;
-          }
-          this.newTask.id = id(10);
-          this.$store.commit('addNewTask',this.newTask);
-          this.closeForm();
+    closeForm() {
+      this.$store.commit("toggleForm");
+    },
+    clearForm() {
+      this.newTask.id = this.newTask.name = "";
+      this.newTask.status = true;
+    },
+    addNewTask() {
+      const id = require("crypto-random-string");
+      if (this.newTask.status === "false") {
+        this.newTask.status = false;
+      } else {
+        this.newTask.status = true;
       }
+      this.newTask.id = id(10);
+      this.$store.commit("addNewTask", this.newTask);
+      this.closeForm();
+    }
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,39 +5,46 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    isDisplayForm : false,
-    tasks : [],
-    editTask : {},
+    isDisplayForm: false,
+    tasks: [],
+    editTask: {},
+    filterNameLowerCase: '',
+    
   },
   mutations: {
-    toggleForm(state){
+    toggleForm(state) {
       state.isDisplayForm = !state.isDisplayForm;
     },
-    generateData(state,tasks){
+    generateData(state, tasks) {
       state.tasks = tasks;
     },
-    toggleStatus(state,id){
+    toggleStatus(state, id) {
       state.tasks.forEach(task => {
-        if(task.id === id){
-          task.status =!task.status;
+        if (task.id === id) {
+          task.status = !task.status;
         }
       })
     },
-    addNewTask(state,newTask){
+    addNewTask(state, newTask) {
       state.tasks.push(newTask);
     },
-    deleteTask(state,index){
-      state.tasks.splice(index,1);
+    deleteTask(state, index) {
+      state.tasks.splice(index, 1);
     },
-    editTask(state,editTask){
+    editTask(state, editTask) {
       state.editTask = editTask;
       state.isDisplayForm = true;
-      // console.log(state.editTask);
+    },
+    filter(state,filterNameLowerCase){
+      state.filterNameLowerCase = filterNameLowerCase;
     }
   },
   actions: {
 
+  },
+  getters: {
+    tasksFilter(state){
+      return state.tasks.filter(task => task.name.toLowerCase().indexOf(state.filterNameLowerCase) !== -1)
+    }
   }
 });
-
-
